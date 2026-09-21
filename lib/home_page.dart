c8320a93fd55ea94894595816fabcdda333828e2
DMzRs <dmrashidpferrer@gmail.com>
2026-09-22 07:51:30 +0800

    Task 7: Firebaase Google Signin

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: const Text('Firebase | Ferrer Task 6'),
+        title: const Text('Firebase | Ferrer Task 7'),
         centerTitle: true,
         backgroundColor: Colors.teal,
         actions: [
