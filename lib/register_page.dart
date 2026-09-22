636c78f6027d2fbb5d4bb9f10121590498099c25
DMzRs <dmrashidpferrer@gmail.com>
2026-09-22 14:34:08 +0800

    Task 7: Firebase Google Signin

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -25,7 +25,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Register")),
+      appBar: AppBar(title: const Text("Register Ferrer")),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16),
